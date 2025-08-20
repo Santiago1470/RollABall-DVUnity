996f3f197ec9c172afbd0fc422fb4f400402c383
Santiago Torres <st147086@gmail.com>
2025-08-19 20:11:02 -0500

    Ajustes clase 19 de agosto
    Mover jugador, agregar paredes y objeto recolectable (sin función)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        // float moveJump = Input.GetAxis("Jump");
+        // float high = 0;
+        // if(moveJump) {
+        //     high = 5;
+        // }
+        // Debug.Log(moveJump);
+        Vector3 movimiento = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+
+        rb.AddForce(movimiento);
+    }
+}
